aef6bfe9ab4e4809402b7074f6fbe48764d99d42
Ubuntu <student@AndroidDevS3.adh0tapeeaseda4tlrbh1gexfe.ax.internal.cloudapp.net>
2021-11-19 16:10:51 +0000

    ukonczone lab6

--- app/src/main/java/com/example/basiclab/SecondFragment.java
+++ app/src/main/java/com/example/basiclab/SecondFragment.java
@@ -29,11 +29,19 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        binding.buttonNext2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_thirdFragment);
+            }
+        });
+
+        binding.buttonPrevious2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
     }
